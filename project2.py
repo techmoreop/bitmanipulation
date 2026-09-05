2772b04c7e881b2eb5c888a8f4e22ceb8d6ecd51
techmoreop <techmoreop@gmail.com>
2026-09-05 22:22:46 +0530

    Add bitwise operations examples in project2.py
    Fix print statements and correct typos in bitwise operations.

---
+++ project2.py
@@ -0,0 +1,13 @@
+print(" 12 =", bin(12)[2:])
+print("not 12 =", ~12 &0xff)
+
+print(" XOR - DIFFERENT bit give 1")
+print("12 ^ 10 =", 12 ^ 10)
+
+ptint("Left shift - multiples  by 2")
+print("", 7, "<< 1 =", 7<<1)
+
+ptint("Rigtht shift - divides by 2")
+print("", 7, ">> 1 =", 7>>1)
+
+print("10|2 bitwise or", 10|2)
